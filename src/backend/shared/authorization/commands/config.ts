a7c96424210d8d7ae263a2018e63d7d391c84424
Sale <sale.radenkovic@gmail.com>
2025-06-07 02:22:21 +0200

    Reformatted authorization lib using prettier.

--- src/backend/shared/authorization/commands/config.ts
+++ src/backend/shared/authorization/commands/config.ts
@@ -6,26 +6,30 @@ const projectRoot = path.resolve(__dirname, '../../');
 const baseDir = path.resolve(__dirname, '../../../../');
 
 const clientProjects: IClientProject[] = [
-    {
-        name: 'Authentication Service',
-        packageJsonPath: path.join(baseDir, 'authentication/package.json'),
-        targetLibsDir: path.join(baseDir, 'authentication/shared/libs'),
-    },
+  {
+    name: 'Authentication Service',
+    packageJsonPath: path.join(baseDir, 'authentication/package.json'),
+    targetLibsDir: path.join(baseDir, 'authentication/shared/libs'),
+  },
 ];
 
 const pkgJsonFiles = ['dist/infrastructure/**/*', 'README.md'];
 
 const tsConfigContent = {
-    extends: './tsconfig.json',
-    // !important: must specify infrastructure dir, ./**/*.ts wont work
-    include: ['./infrastructure/**/*.ts'],
-    exclude: ['./commands/**/*.ts', 'node_modules'],
+  extends: './tsconfig.json',
+  // !important: must specify infrastructure dir, ./**/*.ts wont work
+  include: ['./infrastructure/**/*.ts'],
+  exclude: ['./commands/**/*.ts', 'node_modules'],
 };
 
 export const publishOptions: IPublishOptions = {
-    projectRoot,
-    clientProjects,
-    pkgJsonFiles,
-    tsConfigContent,
-    newVersion: undefined,
+  projectRoot,
+  clientProjects,
+  pkgJsonFiles,
+  tsConfigContent,
+  publishedLibName: 'authorization',
+  newVersion: undefined,
+  selectiveCompilation: false,
+  cleanupTempDist: true,
+  outputDest: './_versions',
 };
